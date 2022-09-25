2951bbe970b60768a6f4c6da7d93c1a03034fe38
ishika <ishika.jain@iitg.ac.in>
2022-09-25 17:04:25 +0530

    online database added

--- config.php
+++ config.php
@@ -3,10 +3,14 @@
 This file contains database configuration assuming you are running mysql using user "root" and password ""
 */
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'login');
+// define('DB_SERVER', 'localhost');
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', '');
+// define('DB_NAME', 'login');
+define('DB_SERVER', 'remotemysql.com');
+define('DB_USERNAME', 'PHVBfNkZnT');
+define('DB_PASSWORD', 'C9xY0sIjtE');
+define('DB_NAME', 'PHVBfNkZnT');
 
 // Try connecting to the Database
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
