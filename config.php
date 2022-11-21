591a20e155100962bcb1fd7559a078dd80bd86d2
ishikajain029 <ishika.jain@iitg.ac.in>
2022-11-21 20:12:03 +0530

    online db

--- config.php
+++ config.php
@@ -7,10 +7,10 @@ This file contains database configuration assuming you are running mysql using u
 // define('DB_USERNAME', 'root');
 // define('DB_PASSWORD', '');
 // define('DB_NAME', 'login');
-define('DB_SERVER', 'remotemysql.com');
-define('DB_USERNAME', 'PHVBfNkZnT');
-define('DB_PASSWORD', 'C9xY0sIjtE');
-define('DB_NAME', 'PHVBfNkZnT');
+define('DB_SERVER', 'sql12.freemysqlhosting.net');
+define('DB_USERNAME', 'sql12579528');
+define('DB_PASSWORD', 'ZG6iiuIsMR');
+define('DB_NAME', 'sql12579528');
 
 // Try connecting to the Database
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
